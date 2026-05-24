9a30ea2d4672415bca0b184c5922e8135eff01aa
Vinayak <vinayak.reddy@wizergos.com>
2026-05-24 21:56:47 +0530

    main changed

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func coursePage(w http.ResponseWriter, r *http.Request) {
 	// Render the course html page
 	http.ServeFile(w, r, "static/courses.html")
-	
 }
 
 func aboutPage(w http.ResponseWriter, r *http.Request) {
